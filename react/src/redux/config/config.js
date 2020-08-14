30714a1eafe127158d502e2054f09ab97196ff68
Mariano Valdez <anopa@localhost.localdomain>
2020-08-14 13:10:10 -0300

    Cambio react

--- react/src/redux/config/config.js
+++ react/src/redux/config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    // baseUrl:'http://dc.ianux.com:8000/'
+    baseUrl:'http://dc.ianux.com:8000/'
     // baseUrl: 'http://localhost:8000/'
-    baseUrl: 'http://0.0.0.0:8000/'
     // baseUrl: 'https://admin-cu.com/'
 }
