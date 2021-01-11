e865d31bddef9dc973668879bcd6ba8bd1266c29
Mariano Valdez <anopa@localhost.localdomain>
2021-01-11 19:40:42 -0300

    config react

--- react/src/redux/config/config.js
+++ react/src/redux/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // baseUrl:'http://dc.ianux.com:8000/'
-    baseUrl: 'http://localhost:8000/'
-    // baseUrl: 'http://admin-cu.com:8000/'
+    // baseUrl: 'http://localhost:8000/'
+    baseUrl: 'http://admin-cu.com:8000/'
 }
