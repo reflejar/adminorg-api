6eb09eb592d289a54bcfbc586b6ed7ef08f15706
Mariano Valdez <anopa@localhost.localdomain>
2021-03-02 11:42:43 -0300

    recibos

--- django/admincu/operative/admin/own_receipt.py
+++ django/admincu/operative/admin/own_receipt.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib import messages
+from import_export.admin import ImportExportMixin
 from admincu.operative.models import OwnReceipt
 
 
-class OwnReceiptAdmin(admin.ModelAdmin):
+class OwnReceiptAdmin(ImportExportMixin, admin.ModelAdmin):
 	list_display = ['document_type', 'formatted_number', 'total_amount']
 	list_filter = ['document_type']
 
