c55f8383991290b33d9bbecd597044c0b1aa71df
Pano Valdez <marianovaldez92@protonmail.com>
2023-09-19 19:53:16 -0300

    sacamos sentry

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -129,26 +129,3 @@ REACT = {
     'dom': 'assets/js/react/react-dom.production.min.js',
     'babel': 'assets/js/react/babel.min.js'
 }
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-	dsn="https://87bd862b6e0e43c881b9059f5920918a@o1151668.ingest.sentry.io/6228979",
-	integrations=[DjangoIntegration()],
-
-	# Set traces_sample_rate to 1.0 to capture 100%
-	# of transactions for performance monitoring.
-	# We recommend adjusting this value in production,
-	traces_sample_rate=1.0,
-
-	# If you wish to associate users to errors (assuming you are using
-	# django.contrib.auth) you may enable sending PII data.
-	send_default_pii=True,
-
-	# By default the SDK will try to use the SENTRY_RELEASE
-	# environment variable, or infer a git commit
-	# SHA as release, however you may want to set
-	# something more human-readable.
-	# release="myapp@1.0.0",
-)
