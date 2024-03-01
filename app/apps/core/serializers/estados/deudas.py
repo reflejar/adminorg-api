262d756b808b5acefb722f319f268177b167cbc6
Pano Valdez <marianovaldez92@protonmail.com>
2024-02-29 21:28:28 -0300

    a punto de terminar cuentas a cobrar

--- app/apps/core/serializers/estados/deudas.py
+++ app/apps/core/serializers/estados/deudas.py
@@ -3,16 +3,16 @@ from .base import *
 class EstadoDeudasSerializer(EstadoBaseSerializer):
 
 
-	def saldo(self, monto, pago_capital, interes, descuento):
+	def saldo(self, monto, pago_capital):#, interes, descuento):
 
-		return monto - pago_capital + interes - descuento
+		return monto - pago_capital# + interes - descuento
 
 	def makeJSON(self, o:Operacion) -> Dict[str, Any]:
 
 
 		pago_capital = o.pago_capital(fecha=self.context['end_date'])
-		interes = o.interes(fecha=self.context['end_date'])
-		descuento = o.descuento(fecha=self.context['end_date'])
+		# interes = o.interes(fecha=self.context['end_date'])
+		# descuento = o.descuento(fecha=self.context['end_date'])
 		receipt_type = str(o.documento.receipt.receipt_type)
 		formatted_number = str(o.documento.receipt.formatted_number)
 
@@ -30,10 +30,10 @@ class EstadoDeudasSerializer(EstadoBaseSerializer):
 			}, 
 			'nombre': receipt_type + " " + formatted_number,
 			'cuenta': str(o.cuenta),
-			'concepto': str(o.concepto or '-') , 
+			'concepto': " - ".join([str(o.concepto or "") ]), 
 			'periodo': o.periodo(),
 			'monto': o.monto,
 			'pago_capital': pago_capital,
-			'interes': interes,
-			'saldo': self.saldo(o.monto, pago_capital, interes, descuento)
+			# 'interes': interes,
+			'saldo': self.saldo(o.monto, pago_capital)#, interes, descuento)
 		}
